ba167d964ae969021e7de261647aef2f37a5f87e
ninhTKieen <ninhtrungkien2811@gmail.com>
2023-06-20 00:20:37 +0700

    feat: config reactotron

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,5 +1,6 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import authReducer from './auth/auth.slice';
+import reactotron from 'reactotronConfig';
 
 const rootReducer = combineReducers({
   auth: authReducer,
@@ -7,6 +8,7 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
+  enhancers: [reactotron.createEnhancer!()],
 });
 
 export type AppDispatch = typeof store.dispatch;
