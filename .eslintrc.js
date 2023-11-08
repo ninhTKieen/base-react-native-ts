5d39390278de285e8d1f0fbcdb6790f9c51af5f9
ninhTKieen <ninhtrungkien2811@gmail.com>
2023-11-08 17:18:55 +0700

    style: no explicit any

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
         'no-undef': 'off',
         '@typescript-eslint/no-unused-vars': 'warn',
         'react-native/no-inline-styles': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
       },
     },
   ],
